3e489b95c3c2f832c6ebcf7a0ea0c1ce3acbdcc1
D4NILO07 <161032185+D4NILO07@users.noreply.github.com>
2024-09-13 09:54:42 -0300

    Update main.py

--- Atividade Pontuada - Presença/main.py
+++ Atividade Pontuada - Presença/main.py
@@ -10,23 +10,23 @@ from models.Cliente import Cliente
 from models.Juridica import Juridica
 system("cls||clear")
 
-endereco_medico = Endereco("Rua BEIA", "130", "1º Andar", "40711 - 600", "Salvador", UnidadeFederativa.BAHIA.nome)
-endereco_motoboy = Endereco("Avenida Paulista", "1000", "Apartamento 202", "01310-100", "São Paulo", UnidadeFederativa.SAO_PAULO.nome)
-endereco_advogado = Endereco("Rua do Comércio", "500", "Sala 101", "20010-000", "Rio de Janeiro", UnidadeFederativa.RIO_DE_JANEIRO.nome)
-endereco_cliente = Endereco("Rua das Palmeiras", "45", "Bloco B", "20250-400", "Rio de Janeiro", UnidadeFederativa.RIO_DE_JANEIRO.nome)
-endereco_juridica = Endereco("Avenida das Nações", "1000", "Conjunto 501", "22222-000", "Rio de Janeiro", UnidadeFederativa.RIO_DE_JANEIRO.nome)
+# Atualização dos endereços
+endereco_medico = Endereco("Rua das Palmeiras", "88", "Apartamento 404", "20250-400", "Rio de Janeiro", UnidadeFederativa.RIO_DE_JANEIRO.nome)
+endereco_motoboy = Endereco("Rua do Comércio", "900", "Sala 501", "20010-000", "Rio de Janeiro", UnidadeFederativa.RIO_DE_JANEIRO.nome)
+endereco_advogado = Endereco("Rua das Nações", "1100", "Andar 4", "22222-000", "Rio de Janeiro", UnidadeFederativa.RIO_DE_JANEIRO.nome)
+endereco_cliente = Endereco("Avenida Paulista", "1234", "Cobertura", "01310-100", "São Paulo", UnidadeFederativa.SAO_PAULO.nome)
+endereco_juridica = Endereco("Rua BEIA", "300", "Bloco C", "40711-600", "Salvador", UnidadeFederativa.BAHIA.nome)
 
+# Atualização das instâncias
+medico = Medico("Rafael Silva", "71 98888-9999", "rafael.silva@medico.com", endereco_medico, "12345678901", "987654321", "12/06/1985", Sexo.MASCULINO, "4567891234567", Setor.ENGENHARIA, 3200.00, "56789")
 
+motoboy = Motoboy("Juliana Santos", "11 99999-2222", "juliana.santos@entrega.com", endereco_motoboy, "10987654321", "123456789", "22/03/1989", Sexo.FEMININO, "404040", Setor.OPERACOES, 1900.00, "5678901234")
 
-medico = Medico("Douglas","71 98472 - 9798","douglas.a.silva7@ba.estudante.senai.br",endereco_medico, "06358631231", "1223131231", "25/03/2007", Sexo.MASCULINO, "1231231313123", Setor.ENGENHARIA, 2532.00, "12312")
+advogado = Advogado("Lucas Pereira", "61 98888-5555", "lucas.pereira@advocacia.com", endereco_advogado, "23456789012", "876543210", "30/08/1980", Sexo.MASCULINO, "987654", Setor.JURIDICO, 5000.00, "DF654321")
 
-motoboy = Motoboy("Carlos Souza","11 98765-4321","carlos.souza@entrega.com",endereco_motoboy,"98765432100","345678912","15/07/1990",Sexo.MASCULINO,"202056",Setor.OPECAOES,1800.00,"1234567890")
+cliente = Cliente("Mariana Costa", "21 98888-7777", "mariana.costa@cliente.com", endereco_cliente, "10987654321", "123456789", "05/09/1991", Sexo.FEMININO, "AT202309004")
 
-advogado = Advogado("Fernanda Oliveira","61 99876-5432","fernanda.oliveira@advocacia.com",endereco_advogado,"12345678901","987654321","23/05/1985",Sexo.FEMININO,"305762",Setor.JURIDICO,4500.00,"DF123456")
-
-cliente = Cliente("Ana Costa","21 99999-8888","ana.costa@exemplo.com",endereco_cliente,"98765432100","321654987","10/11/1992",Sexo.FEMININO,"AT202309001")
-
-juridica = Juridica("Empresa Exemplo LTDA","21 98888-7777","contato@empresaexemplo.com",endereco_juridica,"12.345.678/0001-90","12345678")
+juridica = Juridica("Global Tech LTDA", "21 99999-5555", "contato@globaltech.com", endereco_juridica, "12.345.678/0001-91", "87654321")
 
 print(medico)
 print("\n")
